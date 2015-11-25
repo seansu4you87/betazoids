23979c7c308721bab680ccc91ce8c5a794cc44e3
Sean Yu <yu@squareup.com>
2015-11-25 15:47:55 -0800

    Idempotence#create all done
    Takes a `:before_callback` option, will create idempotently.  If the
    creation is not actually idempotent, then the `:before_callback` will
    not be called

--- lib/idempotence.ex
+++ lib/idempotence.ex
@@ -1,16 +1,69 @@
 defmodule Idempotence do
-  import Ecto.Query # , only: [from: 1, from: 2, from: 3]
+  defmodule DifferentValuesError do
+    defexception message: "Not idempotent, different values"
+  end
 
-  def create(repo_mod, model_mod, changeset, unique_key, ignored_keys \\ []) do
-    case apply(repo_mod, :insert, [changeset]) do
+  import Ecto.Query
+
+  @doc """
+  Creates idempotently, based on a `unique_key` and `changeset`.
+
+  `opts` contains special options:
+  * `:before_callback` a function to execute before the model is created, not called if creation is not idempotent
+  """
+  def create(repo_mod, model_mod, unique_key, changeset, opts \\ []) do
+    before_callback = opts[:before_callback]
+    transaction_fun = fn ->
+      if before_callback != nil, do: before_callback.()
+      res = case apply(repo_mod, :insert, [changeset]) do
+        {:ok, model} -> model
+        {:error, changeset} -> apply(repo_mod, :rollback, [changeset])
+      end
+    end
+
+    case apply(repo_mod, :transaction, [transaction_fun]) do
       {:ok, model} -> {:ok, %{created: true, model: model}}
       {:error, %Ecto.Changeset{errors: [{^unique_key, "has already been taken"}]}} ->
-        unique_key_value = changeset.changes[unique_key]
-        query = from table in model_mod,
-              where: field(table, ^unique_key) == ^unique_key_value,
-             select: table
-        [model] = apply(repo_mod, :all, [query])
-        {:ok, %{created: false, model: model}}
+        [model] = query_model(repo_mod, model_mod, unique_key, changeset.changes[unique_key])
+        case idempotent?(model, changeset) do
+          true ->
+            if before_callback != nil, do: before_callback.()
+            {:ok, %{created: false, model: model}}
+          {false, diffs} -> raise_on_different_values(diffs)
+        end
     end
+  end
+
+  defp raise_on_different_values(diffs) do
+    message = diffs
+    |> Enum.reduce("Different Values: ", fn({k, v}, m ) ->
+      m <> "#{k}: #{inspect elem(v, 0)} != #{inspect elem(v, 1)}"
+    end)
+    raise DifferentValuesError, message: message
+  end
+
+  defp query_model(repo_mod, model_mod, unique_key, unique_key_value) do
+    query = from table in model_mod,
+          where: field(table, ^unique_key) == ^unique_key_value,
+         select: table
+    apply(repo_mod, :all, [query])
+  end
+
+  defp idempotent?(model, changeset) do
+    diffs = differences(model, changeset)
+    if map_size(diffs) == 0 do
+      true
+    else
+      {false, diffs}
+    end
+  end
+
+  defp differences(model, changeset) do
+    changeset.changes
+    |> Enum.reduce(%{}, fn({k,v}, diffs) ->
+      model_value = Map.get(model, k)
+      if v != model_value, do: diffs = Map.put(diffs, k, {model_value, v})
+      diffs
+    end)
   end
 end
